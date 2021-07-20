d16a24edf05933a814b364c52673dbeae5699aae
SklifCybe <i.s.toaccept@gmail.com>
2021-07-20 21:29:20 +0300

    add react-router-dom, create pages, and create sortPopup

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,41 +1,5 @@
 import React from 'react';
 
-// class Categories extends React.Component {
-//   state = {
-//     activeItem: 0,
-//   };
-
-//   selectedItem = (index) => {
-//     // this.setState({
-//     //   activeItem: index,
-//     // });
-//     this.state.activeItem = index;
-//     console.log(this.state);
-//     this.forceUpdate();
-//   };
-
-//   render() {
-//     const { items } = this.props;
-
-//     return (
-//       <div className="categories">
-//         <ul>
-//           {items.map((name, index) => {
-//             return (
-//               <li
-//                 key={`${name}_${index}`}
-//                 className={`${this.state.activeItem === index ? 'active' : ''}`}
-//                 onClick={() => this.selectedItem(index)}>
-//                 {name}
-//               </li>
-//             );
-//           })}
-//         </ul>
-//       </div>
-//     );
-//   }
-// }
-
 function Categories({ items = [] }) {
   const [activeItem, setActiveItem] = React.useState(0);
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './scss/app.scss';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import App from './App';
+
+import './scss/app.scss';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
